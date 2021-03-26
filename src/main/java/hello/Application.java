5f18fc5d589b79dc928ec9d795165e851a207829
Javier Cano de la Torre <j.cdelatorre@alumnos.upm.es>
2021-03-26 19:39:53 +0100

    make the application executable issue #1

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        /*Esto permite ejecutar la aplicacion de Spring*/
         SpringApplication.run(Application.class, args);
     }
 }
